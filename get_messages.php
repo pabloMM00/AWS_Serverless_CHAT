efe8756b375924b93be17396d930bfce438a076a
pabloMM00 <74919193+pabloMM00@users.noreply.github.com>
2024-10-29 17:30:23 +0100

    Delete get_messages.php

--- get_messages.php
+++
@@ -1,4 +0,0 @@
-<?php
-$pdo = new PDO('mysql:host=datos-usuarios-forms.czqio4e2edcs.eu-central-1.rds.amazonaws.com;dbname=CHAT', 'admin', 'Grisuela%2');
-$stmt = $pdo->query("SELECT username, message, color, timestamp FROM messages ORDER BY timestamp DESC");
-echo json_encode($stmt->fetchAll(PDO::FETCH_ASSOC));
